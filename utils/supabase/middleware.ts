cebc6f6bc04b0115a5aa0bb6c12113b6ebf673ae
Joshua Gretton <jb.gretton@googlemail.com>
2025-07-24 11:00:56 +0100

    Middleware added
    Added nextjs middleware to update sessions to add protection to routes.
    
    Users can not longer access login and signup when they have been authenticated.

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,5 +1,5 @@
 import { createServerClient } from "@supabase/ssr";
-import { NextResponse, type NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function updateSession(request: NextRequest) {
 	let supabaseResponse = NextResponse.next({
@@ -30,7 +30,30 @@ export async function updateSession(request: NextRequest) {
 	);
 
 	// refreshing the auth token
-	await supabase.auth.getUser();
+
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
+	if (
+		user &&
+		(request.nextUrl.pathname.startsWith("/login") ||
+			request.nextUrl.pathname.startsWith("/signup"))
+	) {
+		const url = request.nextUrl.clone();
+		url.pathname = "/dashboard";
+		return NextResponse.redirect(url);
+	}
+	if (
+		!user &&
+		!request.nextUrl.pathname.startsWith("/login") &&
+		!request.nextUrl.pathname.startsWith("/auth") &&
+		!request.nextUrl.pathname.startsWith("/signup")
+	) {
+		// no user, potentially respond by redirecting the user to the login page
+		const url = request.nextUrl.clone();
+		url.pathname = "/login";
+		return NextResponse.redirect(url);
+	}
 
 	return supabaseResponse;
 }
