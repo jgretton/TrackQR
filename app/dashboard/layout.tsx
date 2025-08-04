161f18cad7115ca42e705da80b205ec5908d5e8d
Joshua Gretton <jb.gretton@googlemail.com>
2025-08-04 12:40:20 +0100

    Update layout.tsx
    z-index update to put on top.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -15,7 +15,7 @@ export default function DashboardLayout({
 }>) {
 	return (
 		<>
-			<div className="border-b bg-white shadow-sm fixed w-full">
+			<div className="border-b bg-white shadow-sm fixed w-full z-50">
 				<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 					<div className="flex justify-between items-center py-4">
 						<div className="flex items-center gap-8">
